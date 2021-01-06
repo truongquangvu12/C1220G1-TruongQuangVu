846452eff261076004a95bf6753be73a00a74dca
truongquangvu12 <vu2804@gmail.com>
2021-01-06 14:04:57 +0700

    up up up

---
+++ Li_Thuyet_modu1_tu_soan/dt.js
@@ -0,0 +1,9 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("Enter the width");
+inputheight = prompt("Enter the height");
+let width = parseInt(inputwidth);
+let height= parseInt(inputheight);
+let area =width*height;
+document.write("The area is:"+area);
+
